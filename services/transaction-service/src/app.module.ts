a77b045772cdef78e523d3639d35e23747ac8bac
lurldgbodex <segungbodise@gmail.com>
2026-01-06 16:36:16 +0100

    fix: transaction module registration on app module

--- services/transaction-service/src/app.module.ts
+++ services/transaction-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig), TransactionsModule],
   controllers: [],
-  providers: [TransactionsModule],
+  providers: [],
 })
 export class AppModule {}
