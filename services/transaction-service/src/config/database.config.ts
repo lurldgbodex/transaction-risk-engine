8cc3d9669b7a3f90b0758f2868ff93322c1d0472
lurldgbodex <segungbodise@gmail.com>
2026-01-06 16:35:48 +0100

    chore: add default db config values

--- services/transaction-service/src/config/database.config.ts
+++ services/transaction-service/src/config/database.config.ts
@@ -4,9 +4,9 @@ export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'db',
   autoLoadEntities: true,
   synchronize: true,
 };
